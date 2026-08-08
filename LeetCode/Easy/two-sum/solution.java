dda4e3f8bfe2e62db29f9974b26c75d405e16014
Utkarsh Shukla <utk3136@gmail.com>
2026-08-08 23:51:44 +0530

    ✅ Solved: Two Sum [Easy] — LeetCode

---
+++ LeetCode/Easy/two-sum/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<=nums.length;i++){
+        for(int j=nums.length-1;j>i;j--){
+            if(nums[i]+nums[j]==target){
+                return new int[]{i, j};
+            }
+            }
+        }
+        return new int[]{};
+    }
+}
