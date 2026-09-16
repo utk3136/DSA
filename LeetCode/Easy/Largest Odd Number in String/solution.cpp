c4badbf6c2450f67a5ca15939ed111d4ce94900a
Utkarsh Shukla <utk3136@gmail.com>
2026-09-16 21:25:05 +0530

    Solved: Largest Odd Number in String [Easy]

--- LeetCode/Easy/Largest Odd Number in String/solution.cpp
+++ LeetCode/Easy/Largest Odd Number in String/solution.cpp
@@ -4,13 +4,11 @@ public:
         string str="";
         for(int i=num.size()-1;i>=0;i--){
                 if(int(num[i])%2!=0){
-                    for(int j=0;j<=i;j++){
-                        str.push_back(num[j]);
-                    }
-                    break;
+                    return num.substr(0,i+1);
                 }
                 
         }
-        return str;
+        return "";
+        
     }
 };
